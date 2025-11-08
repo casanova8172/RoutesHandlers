4727e0bb9de2765fc43c3f3ded75f6036b6d02ca
casanova8172 <leelanandkatihar01@gmail.com>
2025-11-08 08:52:09 +0530

    all routes in routes module

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const productRouter = require('./routes/products');
+const categoriesRouter = require('./routes/categories');
+
+
+//Logging middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} request made to ${req.url}`);
+    next();
+})
+
+app.use('/products', productRouter);
+app.use('/categories', categoriesRouter);
+
+// ✅ Wildcard Route (handles undefined routes)
+app.use((req, res) => {
+    res.status(404).send("<h1>404 - Page Not Found</h1>");
+});
+
+app.listen(4000, () => {
+    console.log('🚀 Server is running on http://localhost:4000');
+});
